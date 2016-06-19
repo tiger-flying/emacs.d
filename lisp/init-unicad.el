17b31a2d64a09df6d34a5905b5bf814b0b26b171
Rex Shen <mupingshen@gmail.com>
2016-06-19 09:15:15 +0800

    update url of unicad.el, since it was removed from google-code

--- lisp/init-unicad.el
+++ lisp/init-unicad.el
@@ -5,7 +5,7 @@
 (unless (require 'unicad nil 'noerror)
   (progn
     (mkdir "~/.emacs.d/site-lisp/unicad/")
-    (url-copy-file "https://unicad.googlecode.com/svn/trunk/lisp/unicad.el" "~/.emacs.d/site-lisp/unicad/unicad.el" t)))
+    (url-copy-file "https://github.com/emacsmirror/unicad/raw/master/unicad.el" "~/.emacs.d/site-lisp/unicad/unicad.el" t)))
 
 (provide 'init-unicad)
 ;;; init-unicad ends here
